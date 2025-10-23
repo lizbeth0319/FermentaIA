b45cc2ba571119896c1d38a740affc1a36becb20
lizbeth <lizbethfranco1907@gmail.com>
2025-10-23 11:40:12 -0500

    vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,14 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import tailwind from "@tailwindcss/vite";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "localhost",
     port: 8080,
   },
-  plugins: [react(), tailwind(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    tailwind(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
